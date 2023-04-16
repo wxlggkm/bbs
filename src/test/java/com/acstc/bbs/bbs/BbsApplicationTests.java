13510f2c4727d1339d69846a07b3ae5d3e7ceb3b
chenxi <wxlggkm@163.com>
2023-04-16 11:06:40 +0800

    init repo

---
+++ src/test/java/com/acstc/bbs/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acstc.bbs.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
